a79b778b16b1289444fa4106570f0616dd203f85
elahe01~ <elahe.narimani@gmail.com>
2023-12-15 19:15:29 +0330

    fix

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,7 +1,7 @@
 function Input ({valueState , onChangeHandler}){
     return(
-        <div className="w-full ">
-            <input className="w-[450px] h-[50px] bg-[#F3F3F5] rounded-[15px] outline-none  pl-[20px] color-gray border-[2px] border-gray-650" placeholder="Search..." value={valueState} onChange={onChangeHandler}/>
+        <div className=" w-full">
+            <input className="w-[350px] md:w-[450px] h-[50px] bg-[#F3F3F5] rounded-[15px] outline-none  pl-[20px] color-gray border-[2px] border-gray-650" placeholder="Search..." value={valueState} onChange={onChangeHandler}/>
         </div>
     )
 }
